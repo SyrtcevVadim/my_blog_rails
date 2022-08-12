9631aea5b498ab1d5adc3c18ad4ac9eb53c2ba21
Сырцев Вадим Игоревич <syrtcevvi@gmail.com>
2022-08-12 23:31:20 +0300

    Добавлен гем FriendlyId и реализованы SEO-friendly URL

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,8 @@
 class Article < ApplicationRecord
     include Visible
-    
+    extend FriendlyId
+    friendly_id :title, use: :slugged    
+
     has_many :comments, dependent: :delete_all
 
     validates :title, presence: true
